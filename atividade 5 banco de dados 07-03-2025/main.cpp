41ee47b2112fa1932eef881455862d5c32b28428
guilhermeferreira-lab <gui.f765145@gmail.com>
2025-03-07 22:20:57 -0300

    exercicos da aula do dia 07-03-2025

---
+++ atividade 5 banco de dados 07-03-2025/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float valor, finalvalor, reajuste;
+    int me, pagamento;
+
+    cout << "qual o preço do produto?" << endl;
+    cin >> valor;
+
+    cout<<"qual será a forma de pagamento?" << endl;
+    cout<<"À vista em dinheiro ou Pix, com 10% de desconto  (1)" << endl;
+    cout<<"À vista em dinheiro ou Pix, com 5% de desconto   (2)" << endl;
+    cout<<"Em duas vezes ,preço normal da etiqueta sem jutos(3)" << endl;
+    cout<<"Em três vezes, preço de etiqueta com acréscimo de 10%(4)" << end;
+    cin << me;
+
+    switch (me)
+    {
+    case 1:
+        cout <<"escolha uma forma de pagamento :"<< endl;
+        cout <<"dinheiro (1)" << endl;
+        cout <<"Pix      (2)" << endl;
+        cin >> pagamento;
+
+        switch(pagamento)
+        {
+        case 1:
+            reajuste = valor * 10/100;
+            finalvalor = valor - reajuste;
+
+        cout <<"esse é o desconto :" << finalvalor << endl;
+        break;
+
+        case 2:
+            reajuste = valor * 10/100;
+            finalvalor = valor - reajuste;
+
+        cout <<"esse é o desconto :" << finalvalor << endl;
+        break;
+        }
+        case 2:
+             cout <<"escolha uma forma de pagamento :"<< endl;
+        cout <<"dinheiro (1)" << endl;
+        cout <<"Pix      (2)" << endl;
+        cin >> pagamento;
+
+        switch(pagamento)
+        {
+        case 1:
+            reajuste = valor * 10/100;
+            finalvalor = valor - reajuste;
+
+        cout <<"esse é o desconto :" << finalvalor << endl;
+        break;
+
+        case 2:
+            reajuste = valor * 10/100;
+            finalvalor = valor - reajuste;
+
+        cout <<"esse é o desconto :" << finalvalor << endl;
+        break;
+    }
+        case 3:
+            finalvalor = valor / 2;
+
+            cout << "valor das duas parcelas :"<< finalvalor << endl;
+            break;
+}
